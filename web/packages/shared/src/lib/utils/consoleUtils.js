8556bb6bc473e03673e6256548760d23ac18e22a
Ivan Volozhenin <ivan.volozhenin@ideaportriga.com>
2025-11-06 16:08:11 +0200

    fix lints

--- web/packages/shared/src/lib/utils/consoleUtils.js
+++ web/packages/shared/src/lib/utils/consoleUtils.js
@@ -3,5 +3,7 @@ const noConsoleDebug = process.env.VUE_APP_NO_CONSOLE_DEBUG === 'true'
 export let debug = console.debug
 
 if (noConsoleDebug) {
-  debug = () => {}
+  debug = () => {
+    // Debug is disabled
+  }
 }
