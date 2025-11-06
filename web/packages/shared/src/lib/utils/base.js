1f75a708a832c22ef1d30ecb89c6d025ad0409f9
Ivan Volozhenin <ivan.volozhenin@ideaportriga.com>
2025-11-06 16:08:02 +0200

    fix lints

--- web/packages/shared/src/lib/utils/base.js
+++ web/packages/shared/src/lib/utils/base.js
@@ -37,7 +37,7 @@ const toKebabCase = (str) =>
     .join('-')
 
 function getUrlParameter(location, name, decodeConcatSymbol = true) {
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]')
+  name = name.replace(/\[/, '\\[').replace(/\]/, '\\]')
   const regex = new RegExp('[\\?&]' + name + '=([^&#]*)')
   const results = regex.exec(location)
   if (results === null) {
@@ -157,14 +157,14 @@ const newShade = (hexColor, magnitude) => {
   if (hexColor.length === 6) {
     const decimalColor = parseInt(hexColor, 16)
     let r = (decimalColor >> 16) + magnitude
-    r > 255 && (r = 255)
-    r < 0 && (r = 0)
+    if (r > 255) r = 255
+    if (r < 0) r = 0
     let g = (decimalColor & 0x0000ff) + magnitude
-    g > 255 && (g = 255)
-    g < 0 && (g = 0)
+    if (g > 255) g = 255
+    if (g < 0) g = 0
     let b = ((decimalColor >> 8) & 0x00ff) + magnitude
-    b > 255 && (b = 255)
-    b < 0 && (b = 0)
+    if (b > 255) b = 255
+    if (b < 0) b = 0
     return `#${(g | (b << 8) | (r << 16)).toString(16)}`
   } else {
     return hexColor
