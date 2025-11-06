d92330a14fbd0c930accbc76467820a57898209b
Ivan Volozhenin <ivan.volozhenin@ideaportriga.com>
2025-11-06 10:53:46 +0200

    remove lint comm

--- web/apps/@ipr/magnet-admin/cypress/support/commands.ts
+++ web/apps/@ipr/magnet-admin/cypress/support/commands.ts
@@ -13,7 +13,6 @@
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface Chainable<Subject> {
       login(email: string, password: string): void
       g(selector: string): Chainable<JQuery<HTMLElement>>
