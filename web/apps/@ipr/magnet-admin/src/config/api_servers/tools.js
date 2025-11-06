1b92ca0b12a2515ce3151080fe7ff13899a75008
Ivan Volozhenin <ivan.volozhenin@ideaportriga.com>
2025-11-06 10:55:26 +0200

    remove unused imports

--- web/apps/@ipr/magnet-admin/src/config/api_servers/tools.js
+++ web/apps/@ipr/magnet-admin/src/config/api_servers/tools.js
@@ -1,7 +1,7 @@
 import NameDescription from '@/config/assistant-tools/component/NameDescription.vue'
 import { ChipCopy } from '@ui'
 import { markRaw } from 'vue'
-import { formatDateTime } from '@shared/utils/dateTime'
+// import { formatDateTime } from '@shared/utils/dateTime'
 
 const controls = {
   name: {
@@ -28,38 +28,7 @@ const controls = {
     action: 'drilldown',
     align: 'left',
   },
-  // created_at: {
-  //   name: 'created_at',
-  //   label: 'Created',
-  //   field: 'created_at',
-  //   display: true,
-  //   readonly: true,
-  //   fromMetadata: true,
-  //   type: 'Date',
-  //   format: (val) => formatDateTime(val),
-  //   sort: (a, b) => {
-  //     const dateObjectA = new Date(a)
-  //     const dateObjectB = new Date(b)
-  //     return dateObjectA - dateObjectB
-  //   },
-  //   align: 'left',
-  // },
-  // updated_at: {
-  //   name: 'modified_at',
-  //   label: 'Last Updated',
-  //   field: 'modified_at',
-  //   display: true,
-  //   readonly: true,
-  //   fromMetadata: true,
-  //   type: 'Date',
-  //   format: (val) => formatDateTime(val),
-  //   sort: (a, b) => {
-  //     const dateObjectA = new Date(a)
-  //     const dateObjectB = new Date(b)
-  //     return dateObjectA - dateObjectB
-  //   },
-  //   align: 'left',
-  // },
+
 }
 
 export default controls
