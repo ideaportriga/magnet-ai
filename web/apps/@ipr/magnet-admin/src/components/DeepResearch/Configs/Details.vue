81a9d8198249bf50bca66c9c57450ef253891f75
rzipr <romans.zuks@ideaportriga.com>
2026-02-20 19:05:32 +0200

    fix(deep-research): fix name and system name updating

--- web/apps/@ipr/magnet-admin/src/components/DeepResearch/Configs/Details.vue
+++ web/apps/@ipr/magnet-admin/src/components/DeepResearch/Configs/Details.vue
@@ -9,15 +9,16 @@
         .row.items-center.q-gap-12.no-wrap.full-width.q-mt-lg.q-mb-sm.bg-white.border-radius-8.q-py-12.q-px-16
           .col
             .row.items-center
-              km-input-flat.km-heading-4.full-width.text-black(placeholder='Name', v-model='name')
+              km-input-flat.km-heading-4.full-width.text-black(placeholder='Name', :model-value='name', @change='name = $event')
             .row.items-center
-              km-input-flat.km-description.full-width.text-black(placeholder='Description', v-model='description')
+              km-input-flat.km-description.full-width.text-black(placeholder='Description', :model-value='description', @change='description = $event')
             .row.items-center.q-pl-6
               q-icon.col-auto(name='o_info', color='text-secondary')
                 q-tooltip.bg-white.block-shadow.text-black.km-description(self='top middle', :offset='[-50, -50]') System name serves as unique record id
               km-input-flat.col.km-description.full-width(
                 placeholder='Enter system name',
-                v-model='system_name',
+                :model-value='system_name',
+                @change='system_name = $event',
                 @focus='showInfo = true',
                 @blur='showInfo = false'
               )
